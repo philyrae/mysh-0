58f09fa20cfcd3eda9a04a0b0f5c4aa58cf3b4b3
philyrae <philyrae@gmail.com>
2017-10-09 20:34:27 +0900

    update src/util.c and src/commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include "commands.h"
 
@@ -7,6 +9,17 @@ int do_cd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
+  int result;
+  result = chdir(argv[1]);
+  
+  if(result == 0){
+  	
+  }
+  else if(result == -1){
+  	printf("No such file or direcroty\n");
+  }
+  
+
   return 0;
 }
 
@@ -15,16 +28,74 @@ int do_pwd(int argc, char** argv) {
     return -1;
 
   // TODO: Fill it!
+  char buf[2048];
+
+	getcwd(buf, 2048);
+	printf("%s\n", buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  int i;
+  
+  if(argc == 1){
+  	printf("there is not a specific argv.\n");
+  	for(i = 0; i < argc; i++){
+  		printf("%s ", argv[i]);
+  	}
+  	printf("\n");
+  	return 0;
+  }
+  else if(argc == 2){
+  	if(strcmp(argv[0], "cd")){
+  		printf("Wrong command.\n");
+  		for(i = 0; i < argc; i++){
+  			printf("%s ", argv[i]);
+  		}
+  		printf("\n");
+  		return 0;
+  	}
+  	else{
+  		printf("validation pass\n");
+  		return 1;
+  	}
+  }
+  else{
+  	printf("there are too many argv.\n");
+  	for(i = 0; i < argc; i++){
+  		printf("%s ", argv[i]);
+  	}
+  	printf("\n");
+  	return 0;
+  }
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  int i;
+  
+  if(argc == 1){
+  	if(strcmp(argv[0], "pwd")){
+  		printf("Wrong command.\n");
+  		for(i = 0; i < argc; i++){
+  			printf("%s ", argv[i]);
+  		}
+  		printf("\n");
+  		return 0;
+  	}
+  	else{
+  		printf("validation pass\n");
+  	 	return 1;
+  	}
+  }
+  else{
+  	printf("there are too many argv.\n");
+  	for(i = 0; i < argc; i++){
+  		printf("%s ", argv[i]);
+  	}
+  	printf("\n");
+  	return 0;
+  }
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  char key[] = {'\n'};
+  char* check;
+  char buffer[8096] = "";
+  char* parser[1024];
+  char* token;
+  int i, flag, pos_null, numOfNull, pos_command;
+  *argc = 0;
+  flag = 0;
+  numOfNull = 0;
+  pos_command = 0;
+  
+  for(i = 0; i < 1024; i++){
+		parser[i] = malloc(1024);
+  }
+  
+  check = strpbrk(command, key);
+  
+  while(check != NULL){
+  	numOfNull++;
+  	pos_null = (int)(check - command);
+  	check = strpbrk(check+1, key);
+  }
+  
+  if(numOfNull == 0){
+  	strncpy(buffer, command, strlen(command));
+  }
+  else{  	
+  	if(numOfNull == 1 && pos_null == strlen(command)-1){
+  		strncpy(buffer, command, strlen(command)-1);
+  	}
+  	else{
+  		strncpy(buffer, command, strlen(command));
+  	}
+  }
+  
+  token = strtok(buffer, " ");
+  
+  while(token != NULL){
+  	if(flag == 0){
+	  	pos_command++;
+	  }
+  	if((!(strcmp(token, "pwd")) || !(strcmp(token, "cd")))){
+  		flag = 1;
+  	}
+  	strcpy(parser[*argc], token);
+  	(*argc)++;
+  	
+  	token = strtok(NULL, " ");
+  }
+  
+  printf("pos_com: %d\n", pos_command);
+  printf("(*argc): %d\n", (*argc));
+  
+  if(flag == 1){
+  	pos_command -= 1;
+  }
+  
+  (*argc) -= pos_command;
+  
+  printf("(*argc): %d\n", (*argc));
+  
+  *argv = (char **)malloc(sizeof(char **) * (*argc));
+  if(*argc == 0){
+  	(*argv)[0] = (char *)malloc(strlen("") * sizeof(char*));
+  	strcpy((*argv)[0], "");
+  	(*argc)++;
+  }
+  else{
+  	for(i = 0; i < *argc; i++){
+  		(*argv)[i] = (char *)malloc(strlen(parser[pos_command]) * sizeof(char*));
+  		strcpy((*argv)[i], parser[pos_command]);
+  		pos_command++;
+  	}
+  }
+  
+  for(i = 0; i < 1024; i++){
+		free(parser[i]);
+  }  
+  
 }
